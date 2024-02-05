b0bac3c544d5cce2193f3c2e7466196234f21826
Mohamed Fairos Mohamed Ijlal <150043878+MohamedIjlal27@users.noreply.github.com>
2024-02-05 22:57:50 +0530

    2nd

---
+++ App.js
@@ -0,0 +1,28 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './Screens/HomeScreen';
+import Splash from './Components/Splash';
+import Search from './Components/Search';
+import NewsViewer from './Components/NewsView';
+
+const App = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="NewsView" component={NewsViewer} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
